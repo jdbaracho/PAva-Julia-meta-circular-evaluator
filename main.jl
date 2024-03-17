57feb9e7765ffc84477c773b6ed99946f38c199a
Martim Moniz <martim@moniz.pt>
2024-03-17 23:00:48 +0000

    custom functions inside let clauses

--- main.jl
+++ main.jl
@@ -11,11 +11,26 @@ function metajulia_eval(expr, env)
 		if is_let(expr)
 			eval_let(expr, env)
 		elseif is_call(expr)
+			# Evaluate/Resolve the argument values
 			resolved_args = map((arg) -> metajulia_eval(arg, env), expr.args[2:end])
 			call_symbol = expr.args[1]
-			call_func = getfield(Base, call_symbol)
-
-			call_func(resolved_args...)
+			try
+				# Try to find the function in base Julia
+				call_func = getfield(Base, call_symbol)
+				call_func(resolved_args...)
+			catch e
+				if !(e isa UndefVarError)
+					rethrow(e)
+				end
+				# Try to find the function definition in the environment
+				call_pair = get_custom_function(call_symbol, env)
+				# Map argument symbols to argument values (resolved_args) in function block environment
+				names = call_pair[1].args[2:end]
+				values = resolved_args
+				extended_env = augment_environment(names, values, env)
+				# Evaluate the function block with the extended environment
+				metajulia_eval(call_pair[2], extended_env)
+			end
 		elseif is_or_operator(expr)
 			metajulia_eval(expr.args[1], env) || metajulia_eval(expr.args[2], env)
 		elseif is_and_operator(expr)
@@ -28,7 +43,7 @@ function metajulia_eval(expr, env)
 			end
 		elseif is_block(expr)
 			# Filter out linenumbernodes
-			lines = filter((arg) -> !(arg isa LineNumberNode), expr.args)
+			lines = filter_linenumbernodes(expr.args)
 			resolved_lines = map((line) -> metajulia_eval(line, env), lines)
 			resolved_lines[end]
 		end
@@ -59,8 +74,11 @@ function eval_let(expr, env)
 	else
 		assignments = [expr.args[1]]
 	end
+
 	names = map((assignment) -> assignment.args[1], assignments)
-	values = map((assignment) -> metajulia_eval(assignment.args[2], env), assignments)
+	# if function definition, dont eval the assignment value (function block)
+	values = map((assignment) -> is_function_definition(assignment) ?
+		assignment.args[2] : metajulia_eval(assignment.args[2], env), assignments)
 	extended_env = augment_environment(names, values, env)
 	# let block
 	metajulia_eval(expr.args[2], extended_env)
@@ -70,12 +88,11 @@ function augment_environment(names, values, env)
 	if length(names) > 1
 		extended_env = augment_environment(names[2:end], values[2:end], env)
 	else 
-		extended_env = env
+		extended_env = copy(env)
 	end
 	pushfirst!(extended_env, (names[1], values[1]))
 end
 
-# This could just be a dictionary :pensive:
 function eval_name(name, env)
 	if isempty(env)
 		error("Unbound name -- EVAL-NAME", name)
@@ -124,4 +141,28 @@ end
 
 function is_incomplete(expr)
 	Meta.isexpr(expr, :incomplete)
+end
+
+function is_function_definition(expr)
+	Meta.isexpr(expr, :(=)) && Meta.isexpr(expr.args[1], :call) && Meta.isexpr(expr.args[2], :block)
+end
+
+function filter_linenumbernodes(args)
+	# Filter out linenumbernodes
+	filter((arg) -> !(arg isa LineNumberNode), args)
+end
+
+function get_custom_function(name, env)
+	if isempty(env)
+		error("Unbound name -- GET-CUSTOM-FUNCTION", name)
+	else
+		# Retrieve first pair (signature, block)
+		curr = first(env)
+		if Meta.isexpr(curr[1], :call) && curr[1].args[1] == name
+			# Found function definition
+			curr
+		else
+			get_custom_function(name, env[2:end])
+		end
+	end
 end
